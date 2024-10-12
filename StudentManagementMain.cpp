d1c1b547eda698940feabfc5f4dacf91ef637888
My Hanh <myhanh13022002@gmail.com>
2024-10-13 00:52:53 +0700

    update a student by id

--- StudentManagementMain.cpp
+++ StudentManagementMain.cpp
@@ -13,5 +13,8 @@ int main(){
     cout<<"\nnhap ma so sinh vien cua sinh vien ma ban muon xoa: "; cin>>studentId;
     studentManagement.removeStudent(studentId);
     cout<<studentManagement;
+    cout<<"\nnhap ma so sinh vien cua sinh vien ma ban muon cap nhat: "; cin>>studentId;
+    studentManagement.updateStudent(studentId);
+    cout<<studentManagement;
     return 0;
 }
